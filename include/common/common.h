60701d82d6053bbef5df9f0efa33de77295a33e0
23wiro <william.1.roos@gmail.com>
2024-06-27 23:18:17 +0200

    working with substeps

--- include/common/common.h
+++ include/common/common.h
@@ -9,14 +9,7 @@ typedef struct {
     vector2 position;
     vector2 velocity;
     vector2 acceleration;
-    vector2 gridPosition;
-    int indexInPointArray;
 } centerPoint;
-
-typedef struct list {
-    int data;
-    struct list *next;
-} list;
 
 typedef struct {
     centerPoint *points;
@@ -26,23 +19,11 @@ typedef struct {
 
 extern unsigned int VBO;
 extern float radius;
-extern int divisionX = 9;
-extern int divisionY = 9;
 
 
-void initPointArray(pointArray *a, int initialSize);
+void initPointArray(pointArray *a, int capacity);
 
 void freePointArray(pointArray *a);
-
-list* addToList(list *head, centerPoint *p);
-
-void addToChunk(centerPoint *p, list ***chunkArray);
-
-int getDataOfIndex(list *head, int index, int *data);
-
-int setDataOfIndex(list *head, int index, int data);
-
-void popFromList(list **head, int index);
 
 void addPoint(pointArray *a, double x, double y, double vx, double vy);
 
@@ -50,7 +31,7 @@ void circleGen(centerPoint *p, float radius, int numSegments, float *vertices);
 
 void drawHollow(centerPoint *p, float radius, int numSegments, unsigned int VBO);
 
-void verlet(centerPoint *p, double dt, int subSteps, float cellWidth, float cellHeight);
+void verlet(centerPoint *p, double dt, int subSteps);
 
 int borderCollision(centerPoint *p, float radius);
 
